bcecf9d92268656d4baf66afe5c93e7f890dfbef
windows <windows@microsoft.com>
2023-11-26 16:43:13 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Myabout()
+{
+    return <h1>welcome to first file yyyyyyyyyyyyyyyyy </h1>
+}
+export default Myabout
+
+
+export function Myabout1()
+{
+    return <h2>This is name export function</h2>
+}
+
+
+export function Myabout2()
+{
+    return <div>
+        <h2>welcome</h2>
+        <p>this is </p>
+    </div>
+}
+
+export function Myabout3()
+{
+    return(
+        <div>
+            <h1>this is heading</h1>
+            <p>this is text</p>
+            <ul>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+            </ul>
+
+        </div>
+
+    )
+}
+
+
+
